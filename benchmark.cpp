ddcd8bc07509c70c37aa96172d0ea25314cc88a5
Wes Bethel <ewbethel@lbl.gov>
2023-08-01 13:59:34 -0700

    Tweak printf format string to eliminate compile warning

--- benchmark.cpp
+++ benchmark.cpp
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
    /* For each test size */
    for (int64_t n : problem_sizes) 
    {
-      printf("Working on problem size N=%d \n", n);
+      printf("Working on problem size N=%lld \n", n);
 
       // invoke user code to set up the problem
       setup(n, &A[0]);
